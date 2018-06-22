ccbaaa25a0a810505d746910490951788aa78fd2
SYNOPSIS <synopsis@MacBook-Pro-de-SYNOPSIS-2.local>
2018-06-22 16:48:30 -0500

    tranferencia look n feel

---
+++ MyNotes/SideMenuFiles/TransferenciasViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TransferenciasViewController.swift
+//
+//  Created by Renzo Alvarado on 22/06/18.
+//  Copyright © 2018 Synopsis. All rights reserved.
+//
+
+import UIKit
+
+class TransferenciasViewController: UIViewController {
+
+    @IBAction fileprivate func close() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
