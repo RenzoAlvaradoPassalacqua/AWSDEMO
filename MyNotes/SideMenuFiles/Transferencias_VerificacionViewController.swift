47dffe5cb445c663b9901d518542268d6deb9b46
SYNOPSIS <synopsis@MacBook-Pro-de-SYNOPSIS-2.local>
2018-06-28 10:45:04 -0500

    cambios

---
+++ MyNotes/SideMenuFiles/Transferencias_VerificacionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Transferencias_VerificacionViewController
+//
+//  Created by Renzo Alvarado on 22/06/18.
+//  Copyright © 2018 Synopsis. All rights reserved.
+//
+
+import UIKit
+
+class Transferencias_VerificacionViewController: UIViewController {
+
+    @IBAction fileprivate func close() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
